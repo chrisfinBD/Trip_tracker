9c620c9f389fee87dd9dc1916ff87c15800def50
britt-c <bhitchman92@gmail.com>
2020-03-14 15:49:26 -0700

    resolved some handlesubmit issues

--- journey/src/App.js
+++ journey/src/App.js
@@ -10,12 +10,14 @@ function App() {
 	const [fav, setFav] = useState(false)
 	const handleFav = (event) => { setFav(!fav) }
   const tripList = trips.map((trip, i) => (
-    <li key={i}>
+    <li className="tripList" key={i}>
       <span>{trip.title} <button onClick={handleFav}>{fav ? "💖" : "♡" }</button></span>
     </li>)
   )
-
-
+	const [tripTitle, setTripTtitle] = useState("")
+	const [locationsValue, setLocationsValue] = useState("")
+	const [startDate, setStartDate] = useState(new Date().getDate())
+	
 	useEffect(() => {
     fetch("http://localhost:3020/trips.json")
       .then((res) => res.json())
@@ -23,13 +25,20 @@ function App() {
       .catch((error) => console.log("Oops", error))
   }, [])
 
+	const handleSubmit = (event)=> {
+		const params = {trips: {title: tripTitle, loctions: locationsValue, start_date: startDate}}
+			fetch(`http://localhost:3020/trips.json`, {
+				method: "POST",
+				body: JSON.stringify(params)
+			}).then((res)=> console.log(res))
+			.catch((err)=> console.log("Oops", err))
+		}
 
   return (
     <div className="App">
       <header className="App-header">
         {tripList}
 				<TripGenerator />
-        
 			</header>
     </div>
   );
--- journey/src/Components/TripGenerator.js
+++ journey/src/Components/TripGenerator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import LocationForm from './LocationForm'
 
 function TripGenerator() {
-	
+
  return (
 		<div>
 		<input placeholder="Name Your Trip"/><br />
