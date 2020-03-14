5a4f54278b22a8fe80dc213960bf858b39541326
chrisfinBD <christie.finnie@gmail.com>
2020-03-14 11:24:45 -0700

    Add seed file. Fix issues caused my missing s in trips. Run rails db:create, rails db:migrate, rails db:seed on initial test.

--- trip_api/db/schema.rb
+++ trip_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_165351) do
+ActiveRecord::Schema.define(version: 2020_03_14_174347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_165351) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "trip_id"
   end
 
   create_table "trips", force: :cascade do |t|
